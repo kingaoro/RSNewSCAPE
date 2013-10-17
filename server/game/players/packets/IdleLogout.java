5d648479729f93ec4b870e0b355a8cf4a199d52a
kingaoro <romneyadrocks@gmail.com>
2013-10-17 21:38:02 +1100

    Updated timer
    Timer now logs out after 5 minutes

--- server/game/players/packets/IdleLogout.java
+++ server/game/players/packets/IdleLogout.java
@@ -3,14 +3,26 @@ package server.game.players.packets;
 
 import server.game.players.Client;
 import server.game.players.PacketType;
+import core.util.Misc;
 
 
 public class IdleLogout implements PacketType {
 
 	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
-		//if (!c.playerName.equalsIgnoreCase("Sanity"))
-		c.logout();
-	    //TODO make a idle timer
+		public void processPacket(Client c, int packetType, int packetSize) {
+                switch (packetType) {
+			case 202:
+			if (c.underAttackBy > 0 || c.underAttackBy2 > 0) {
+				return;
+			} else {
+			if (!c.playerName.equalsIgnoreCase("Mod aoro"));
+				c.logout();
+				Misc.println(c.playerName + " is idle, kicked.");
+		        }
+                        break;
+                }
+  
 	}
 }
+		
+		
